619a329beb481612d3762a10e1b34b77f2748d31
Margaux7 <margaux7222@gmail.com>
2022-07-11 14:58:33 +0800

    chore: update prettier config

--- src/index.tsx
+++ src/index.tsx
@@ -7,146 +7,146 @@ const DEFAULT_MIN_WIDTH = 300;
 const DEFAULT_COL_COUNT = 3;
 
 interface LayoutProps {
-	items?: React.ReactNode[];
-	className?: string;
-	gap?: number;
-	colCount?: number;
-	minWidth?: number;
+  items?: React.ReactNode[];
+  className?: string;
+  gap?: number;
+  colCount?: number;
+  minWidth?: number;
 }
 
 interface ImgLoadStatus {
-	img: HTMLImageElement;
-	loaded: boolean;
+  img: HTMLImageElement;
+  loaded: boolean;
 }
 
 const throttle = (fn: () => void, delay: number) => {
-	let timer: ReturnType<typeof setTimeout> | null;
-	return (...args: any) => {
-		if (timer) return;
-		timer = setTimeout(() => {
-			fn.apply(this, args);
-			timer = null;
-		}, delay);
-	};
+  let timer: ReturnType<typeof setTimeout> | null;
+  return (...args: any) => {
+    if (timer) return;
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+      timer = null;
+    }, delay);
+  };
 };
 
 const Layout: React.FC<LayoutProps> = ({
-	items = [],
-	className,
-	gap = DEFAULT_GAP,
-	colCount = DEFAULT_COL_COUNT,
-	minWidth = DEFAULT_MIN_WIDTH,
+  items = [],
+  className,
+  gap = DEFAULT_GAP,
+  colCount = DEFAULT_COL_COUNT,
+  minWidth = DEFAULT_MIN_WIDTH,
 }: LayoutProps) => {
-	const [spanArray, setSpanArray] = useState<number[]>([]);
-	const [allImgLoaded, setAllImgLoaded] = useState<boolean>(false);
-	const layoutRef = useRef<HTMLDivElement>(null);
+  const [spanArray, setSpanArray] = useState<number[]>([]);
+  const [allImgLoaded, setAllImgLoaded] = useState<boolean>(false);
+  const layoutRef = useRef<HTMLDivElement>(null);
 
-	useLayoutEffect(() => {
-		setAllImgLoaded(false);
+  useLayoutEffect(() => {
+    setAllImgLoaded(false);
 
-		const appendedImgStatusArray = _getAppendedImgLoadStatusArray();
-		// Child containing img appended, should wait for img loading
-		const needWaitImgLoading = !!appendedImgStatusArray.length;
+    const appendedImgStatusArray = _getAppendedImgLoadStatusArray();
+    // Child containing img appended, should wait for img loading
+    const needWaitImgLoading = !!appendedImgStatusArray.length;
 
-		if (needWaitImgLoading) {
-			_waitImgLoading(appendedImgStatusArray);
-		} else {
-			_resizeItems();
-		}
-	}, [items]);
+    if (needWaitImgLoading) {
+      _waitImgLoading(appendedImgStatusArray);
+    } else {
+      _resizeItems();
+    }
+  }, [items]);
 
-	useLayoutEffect(() => {
-		const resizeListener = throttle(() => {
-			_resizeItems();
-		}, 300);
+  useLayoutEffect(() => {
+    const resizeListener = throttle(() => {
+      _resizeItems();
+    }, 300);
 
-		window.addEventListener('resize', resizeListener);
-		return () => {
-			window.removeEventListener('resize', resizeListener);
-		};
-	}, []);
+    window.addEventListener('resize', resizeListener);
+    return () => {
+      window.removeEventListener('resize', resizeListener);
+    };
+  }, []);
 
-	useEffect(() => {
-		if (allImgLoaded) {
-			_resizeItems();
-		}
-	}, [allImgLoaded]);
+  useEffect(() => {
+    if (allImgLoaded) {
+      _resizeItems();
+    }
+  }, [allImgLoaded]);
 
-	const _getClassName = (classNames: string[]) => {
-		return classNames.filter((e) => e).join(' ');
-	};
+  const _getClassName = (classNames: string[]) => {
+    return classNames.filter((e) => e).join(' ');
+  };
 
-	const _getAppendedImgLoadStatusArray = (): ImgLoadStatus[] => {
-		const newImgArr: ImgLoadStatus[] = [];
+  const _getAppendedImgLoadStatusArray = (): ImgLoadStatus[] => {
+    const newImgArr: ImgLoadStatus[] = [];
 
-		if (null !== layoutRef.current) {
-			Array.from(layoutRef.current.children).forEach((el) => {
-				const imgs = el.firstElementChild?.getElementsByTagName('img');
-				if (imgs?.length) {
-					newImgArr.push(
-						...Array.from(imgs)
-							.filter((img) => !img.onload)
-							.map((img) => ({
-								img,
-								loaded: false,
-							}))
-					);
-				}
-			});
-		}
+    if (null !== layoutRef.current) {
+      Array.from(layoutRef.current.children).forEach((el) => {
+        const imgs = el.firstElementChild?.getElementsByTagName('img');
+        if (imgs?.length) {
+          newImgArr.push(
+            ...Array.from(imgs)
+              .filter((img) => !img.onload)
+              .map((img) => ({
+                img,
+                loaded: false,
+              }))
+          );
+        }
+      });
+    }
 
-		return newImgArr;
-	};
+    return newImgArr;
+  };
 
-	const _waitImgLoading = (appendedImgStatusArray: ImgLoadStatus[]): void => {
-		appendedImgStatusArray.forEach((item) => {
-			if (!item.img.onload) {
-				item.img.onload = item.img.onerror = () => {
-					item.loaded = true;
-					if (!appendedImgStatusArray.some((e) => e.loaded === false)) {
-						setAllImgLoaded(true);
-					}
-				};
-			}
-		});
-	};
+  const _waitImgLoading = (appendedImgStatusArray: ImgLoadStatus[]): void => {
+    appendedImgStatusArray.forEach((item) => {
+      if (!item.img.onload) {
+        item.img.onload = item.img.onerror = () => {
+          item.loaded = true;
+          if (!appendedImgStatusArray.some((e) => e.loaded === false)) {
+            setAllImgLoaded(true);
+          }
+        };
+      }
+    });
+  };
 
-	const _resizeItems = () => {
-		if (null !== layoutRef.current) {
-			const newSpanArray: number[] = [];
-			const items = Array.from(layoutRef.current.children);
-			items.forEach((el) => {
-				const itemHeight =
-					el.firstElementChild?.getBoundingClientRect().height || 0;
-				newSpanArray.push(Math.ceil((itemHeight + gap) / gap));
-			});
-			setSpanArray(newSpanArray);
-		}
-	};
+  const _resizeItems = () => {
+    if (null !== layoutRef.current) {
+      const newSpanArray: number[] = [];
+      const items = Array.from(layoutRef.current.children);
+      items.forEach((el) => {
+        const itemHeight =
+          el.firstElementChild?.getBoundingClientRect().height || 0;
+        newSpanArray.push(Math.ceil((itemHeight + gap) / gap));
+      });
+      setSpanArray(newSpanArray);
+    }
+  };
 
-	return (
-		<div
-			className={_getClassName([style.layout, className])}
-			ref={layoutRef}
-			style={{
-				gridTemplateColumns: `repeat(${colCount || 3}, minmax(${
-					minWidth || 300
-				}px,1fr))`,
-				gridGap: `${gap}px`,
-			}}
-		>
-			{items.map((item, i) => {
-				if (React.isValidElement(item)) {
-					return (
-						<Item key={item.key || i} gridSpan={spanArray[i]}>
-							{item}
-						</Item>
-					);
-				}
-				return null;
-			})}
-		</div>
-	);
+  return (
+    <div
+      className={_getClassName([style.layout, className])}
+      ref={layoutRef}
+      style={{
+        gridTemplateColumns: `repeat(${colCount || 3}, minmax(${
+          minWidth || 300
+        }px,1fr))`,
+        gridGap: `${gap}px`,
+      }}
+    >
+      {items.map((item, i) => {
+        if (React.isValidElement(item)) {
+          return (
+            <Item key={item.key || i} gridSpan={spanArray[i]}>
+              {item}
+            </Item>
+          );
+        }
+        return null;
+      })}
+    </div>
+  );
 };
 
 export default Layout;
